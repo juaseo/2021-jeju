4b45770db6a03a154a757de028e0daa53f1f8ee9
juaseo <juaseo96@gmail.com>
2022-01-18 22:35:04 +0900

    main/slide-body ing

--- js/index.js
+++ js/index.js
@@ -1,11 +1,61 @@
-/*************** 글로벌 설정 *****************/
+//*************** main-wrapper *****************/
+$(function() {
 
 
-/*************** 사용자 함수 *****************/
+//*************** 글로벌 설정 *****************/
+var $wrapper = 	$('.main-wrapper')
+var $slide = 	$('.main-wrapper .slide')
+var video = $('.main-wrapper .video')[0]
+var len = $slide.length
+var lastIdx = len - 1
+var depth = 2
+var idx = 0
+var gap = 3000
+var speed = 500
+init()
 
 
-/*************** 이벤트 등록 *****************/
 
 
-/*************** 이벤트 콜백 *****************/
+//*************** 사용자 함수 *****************/
+function init() {
+	$slide.eq(idx).css('z-index', depth++)
+	onAni()
+}
 
+
+//*************** 이벤트 등록 *****************/
+video.addEventListener('loadeddata', onLoadedVideo)
+video.addEventListener('ended', onPlay)
+
+
+
+//*************** 이벤트 콜백 *****************/
+function onLoadedVideo() {
+	if(video.readyState >= 2) {
+		video.playbackRate = 4.0
+	}
+}
+
+function onAni() {
+	video.currentTime = 0
+	if($slide.eq(idx).hasClass('is-video')) video.play()
+	else setTimeout(onPlay, gap)
+}
+
+function onPlay() { 
+	idx = (idx == lastIdx) ? 0 : idx + 1;
+	$slide.eq(idx).css({'z-index': depth++, 'left': '100%'})
+	$slide.eq(idx).stop().animate({'left': 0}, speed, onAni)
+}
+
+
+
+
+
+
+
+
+
+
+})
