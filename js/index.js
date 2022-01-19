5a3a228d8128c237cc0477f91a8350100d3b8555
juaseo <juaseo96@gmail.com>
2022-01-19 18:27:31 +0900

    modal-video done

--- js/index.js
+++ js/index.js
@@ -20,6 +20,7 @@ init()
 //*************** 사용자 함수 *****************/
 function init() {
 	$slide.eq(idx).css('z-index', depth++)
+	$slide.eq(idx).addClass('active')
 	onAni()
 }
 
@@ -27,10 +28,20 @@ function init() {
 //*************** 이벤트 등록 *****************/
 video.addEventListener('loadeddata', onLoadedVideo)
 video.addEventListener('ended', onPlay)
+$('.bt-video').click(onModalVideo)
+$('.modal-video').find('.bt-close').click(onModalVideoClose)
 
 
 
 //*************** 이벤트 콜백 *****************/
+function onModalVideoClose() {
+	$('.modal-video').hide()
+}
+
+function onModalVideo() {
+	$('.modal-video').show()
+}
+
 function onLoadedVideo() {
 	if(video.readyState >= 2) {
 		video.playbackRate = 4.0
@@ -38,6 +49,7 @@ function onLoadedVideo() {
 }
 
 function onAni() {
+	$(this).addClass('active')
 	video.currentTime = 0
 	if($slide.eq(idx).hasClass('is-video')) video.play()
 	else setTimeout(onPlay, gap)
@@ -46,6 +58,7 @@ function onAni() {
 function onPlay() { 
 	idx = (idx == lastIdx) ? 0 : idx + 1;
 	$slide.eq(idx).css({'z-index': depth++, 'left': '100%'})
+	$slide.removeClass('active')
 	$slide.eq(idx).stop().animate({'left': 0}, speed, onAni)
 }
 
