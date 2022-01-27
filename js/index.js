85db34b9a487428aaa1f04d1ca292b75e0882430
juaseo <juaseo96@gmail.com>
2022-01-27 17:48:33 +0900

    weader API done

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@ $(function () {
 	var $slide = $('.main-wrapper .slide')
 	var $pagerSlide = $('.main-wrapper .pager-slide')
 	var video = $('.main-wrapper .video')[0]
+	var $weather = $('.main-wrapper .weather')
 	var len = $slide.length
 	var lastIdx = len - 1
 	var depth = 2
@@ -14,7 +15,33 @@ $(function () {
 	var gap = 5000
 	var speed = 500
 	var timeout
+	var weatherURL = 'https://api.openweathermap.org/data/2.5/weather'
+	var weatherData = {
+		appid: 'a79e2f75b07479b3333c6463a9e30d05',
+		units: 'metric'
+	}
+	var weatherIcon = {
+		i01d: 'bi-brightness-high',
+		i01n: 'bi-brightness-high-fill',
+		i02d: 'bi-cloud-sun',
+		i02n: 'bi-cloud-sun-fill',
+		i03d: 'bi-cloud',
+		i03n: 'bi-cloud-fill',
+		i04d: 'bi-clouds',
+		i04n: 'bi-cloud-fills',
+		i09d: 'bi-cloud-rain-heavy',
+		i09n: 'bi-cloud-rain-heavy-fill',
+		i10d: 'bi-cloud-drizzle',
+		i10n: 'bi-cloud-drizzle-fill',
+		i11d: 'bi-cloud-lightning',
+		i11n: 'bi-cloud-lightning-fill',
+		i13d: 'bi-cloud-snow',
+		i13n: 'bi-cloud-snow-fill',
+		i50d: 'bi-cloud-haze',
+		i50n: 'bi-cloud-haze-fill'
+	}
 	init()
+	weather()
 
 
 
@@ -27,6 +54,10 @@ $(function () {
 			$('<i class="pager" ></i>').appendTo($pagerSlide).click(onPagerClick).addClass((idx === i) ? 'active' : '')
 		}
 		ani()
+	}
+
+	function weather() {
+		navigator.geolocation.getCurrentPosition(onGetGeo, onErrorGeo)
 	}
 
 
@@ -44,6 +75,27 @@ $(function () {
 
 
 	//*************** 이벤트 콜백 *****************/
+	function onGetWeather(r) {
+		console.log(r)
+		$weather.find('.icon').addClass(weatherIcon['i' + r.weather[0].icon])
+		$weather.find('.temp').text(Math.floor(r.main.temp))
+		$weather.find('.date').text(moment(r.dt * 1000).format('YYYY. M. D. ddd'))
+		$weather.find('.time > span').text(moment(r.dt * 1000).format('hh:mm'))
+		$weather.find('.time > small').text(moment(r.dt * 1000).format('A'))
+	}
+
+	function onGetGeo(r) {
+		weatherData.lat = r.coords.latitude
+		weatherData.lon = r.coords.longitude
+		$.get(weatherURL, weatherData, onGetWeather)
+	}
+
+	function onErrorGeo() {
+		weatherData.let = 33.4857397138786
+		weatherData.llon = 126.48154043372092
+		$.get(weatherURL, weatherData, onGetWeather)
+	}
+
 	function onPagerClick() {
 		idx = $(this).index()
 		onPlay('pager')
