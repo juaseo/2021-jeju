df2947708d1fb628560e4e95d3aa2c0ddd7f2dd6
juaseo <juaseo96@gmail.com>
2022-02-14 22:08:04 +0900

    promo-wrapper js ing

--- js/index.js
+++ js/index.js
@@ -1,189 +1,220 @@
-//*************** main-wrapper *****************/
+//*************** index *****************/
 $(function () {
 
-
-	//*************** 글로벌 설정 *****************/
-	var $wrapper = $('.main-wrapper')
-	var $slide = $('.main-wrapper .slide')
-	var $pagerSlide = $('.main-wrapper .pager-slide')
-	var video = $('.main-wrapper .video')[0]
-	var $weather = $('.main-wrapper .weather')
-	var len = $slide.length
-	var lastIdx = len - 1
-	var depth = 2
-	var idx = 0
-	var gap = 5000
-	var speed = 500
-	var timeout
-	var weatherURL = 'https://api.openweathermap.org/data/2.5/weather'
-	var weatherData = {
-		appid: 'a79e2f75b07479b3333c6463a9e30d05',
-		units: 'metric'
-	}
-	var weatherIcon = {
-		i01d: 'bi-brightness-high',
-		i01n: 'bi-brightness-high-fill',
-		i02d: 'bi-cloud-sun',
-		i02n: 'bi-cloud-sun-fill',
-		i03d: 'bi-cloud',
-		i03n: 'bi-cloud-fill',
-		i04d: 'bi-clouds',
-		i04n: 'bi-cloud-fills',
-		i09d: 'bi-cloud-rain-heavy',
-		i09n: 'bi-cloud-rain-heavy-fill',
-		i10d: 'bi-cloud-drizzle',
-		i10n: 'bi-cloud-drizzle-fill',
-		i11d: 'bi-cloud-lightning',
-		i11n: 'bi-cloud-lightning-fill',
-		i13d: 'bi-cloud-snow',
-		i13n: 'bi-cloud-snow-fill',
-		i50d: 'bi-cloud-haze',
-		i50n: 'bi-cloud-haze-fill'
-	}
-	init()
-	weather()
-
-
-
-
-	//*************** 사용자 함수 *****************/
-	function init() {
-		if($.cookie('hideCookie') === 'Y') onCloseCookie()
-		$slide.eq(idx).css('z-index', depth++)
-		$slide.eq(idx).addClass('active')
-		for (var i = 0; i < len; i++) {
-			$('<i class="pager" ></i>').appendTo($pagerSlide).click(onPagerClick).addClass((idx === i) ? 'active' : '')
-		}
-		ani()
-		slideDream()
-	}
-
-	function weather() {
-		navigator.geolocation.getCurrentPosition(onGetGeo, onErrorGeo)
-	}
-
-	function slideDream() {
-		var swiper = new Swiper('.dream-wrapper .swiper-container', {
+	function getSwiperOption(cls, opt) {
+		if(opt)
+		return {
 			pagination: {
-				el: ".dream-wrapper .pager-wrapper",
-				clickable: true,
+				el: '.dream-wrapper .pager-wrapper',
+				clickable: true
 			},
 			navigation: {
-				nextEl: ".dream-wrapper .bt-slide.right",
-				prevEl: ".dream-wrapper .bt-slide.left",
+				nextEl: '.dream-wrapper .bt-slide.right',
+				prevEl: '.dream-wrapper .bt-slide.left',
+			},
+			autoplay: {
+				delay: 3000,
 			},
 			loop: true,
 			slidesPerView: 1,
 			spaceBetween: 40,
 			breakpoints: {
 				576: {
-					slidesPerView: 2
+					slidesPerView: 2,
 				},
 				992: {
-					slidesPerView: 3
-				},
-			},
-		});
+					slidesPerView: 3,
+				}
+			}
+		}
 	}
 
+	setCookie() 
+	weather() 
+	slideMain()
+	slideDream() 
+	slidePromo()
 
-	//*************** 이벤트 등록 *****************/
-	video.addEventListener('loadeddata', onLoadedVideo)
-	video.addEventListener('ended', onPlay)
-	$('.bt-video').click(onModalVideo)
-	$('.modal-video').find('.bt-close').click(onModalVideoClose)
-	$('.cookie-wrapper').find('.bt-confirm').click(onCloseTodayCookie)
-	$('.cookie-wrapper').find('.bt-close').click(onCloseCookie)
-	$('.pager-wrapper .pager').click(function () {
-		$('.pager-wrapper .pager').removeClass('active')
-		$(this).addClass('active')
+	function setCookie() {
+		var $cookieWrapper = $('.cookie-wrapper')
+		var $cookieClose = $cookieWrapper.find('.bt-close')
+		var $cookieCloseConfirm = $cookieWrapper.find('.bt-confirm')
+
+		if ($.cookie('hideCookie') === 'Y') onCloseCookie()
+
+		function onCloseTodayCookie() {
+			$.cookie('hideCookie', 'Y', {
+				expires: 1,
+				path: '/'
+			});
+			onCloseCookie();
+		}
+
+		function onCloseCookie() {
+			$('.cookie-wrapper').hide()
+		}
+
+		$cookieCloseConfirm.click(onCloseTodayCookie)
+		$cookieClose.click(onCloseCookie)
+	}
+
+	function weather() {
+		var $weather = $('.main-wrapper .weather')
+		var weatherURL = 'https://api.openweathermap.org/data/2.5/weather'
+		var weatherData = {
+			appid: 'a79e2f75b07479b3333c6463a9e30d05',
+			units: 'metric'
+		}
+		var weatherIcon = {
+			i01d: 'bi-brightness-high',
+			i01n: 'bi-brightness-high-fill',
+			i02d: 'bi-cloud-sun',
+			i02n: 'bi-cloud-sun-fill',
+			i03d: 'bi-cloud',
+			i03n: 'bi-cloud-fill',
+			i04d: 'bi-clouds',
+			i04n: 'bi-cloud-fills',
+			i09d: 'bi-cloud-rain-heavy',
+			i09n: 'bi-cloud-rain-heavy-fill',
+			i10d: 'bi-cloud-drizzle',
+			i10n: 'bi-cloud-drizzle-fill',
+			i11d: 'bi-cloud-lightning',
+			i11n: 'bi-cloud-lightning-fill',
+			i13d: 'bi-cloud-snow',
+			i13n: 'bi-cloud-snow-fill',
+			i50d: 'bi-cloud-haze',
+			i50n: 'bi-cloud-haze-fill'
+		}
+
+		function onGetWeather(r) {
+			$weather.find('.icon').addClass(weatherIcon['i' + r.weather[0].icon])
+			$weather.find('.temp').text(Math.floor(r.main.temp))
+			$weather.find('.date').text(moment(r.dt * 1000).format('YYYY. M. D. ddd'))
+			$weather.find('.time > span').text(moment(r.dt * 1000).format('hh:mm'))
+			$weather.find('.time > small').text(moment(r.dt * 1000).format('A'))
+		}
+
+		function onGetGeo(r) {
+			weatherData.lat = r.coords.latitude
+			weatherData.lon = r.coords.longitude
+			$.get(weatherURL, weatherData, onGetWeather)
+		}
+
+		function onErrorGeo() {
+			weatherData.let = 33.4857397138786
+			weatherData.llon = 126.48154043372092
+			$.get(weatherURL, weatherData, onGetWeather)
+		}
+		navigator.geolocation.getCurrentPosition(onGetGeo, onErrorGeo)
+	}
+
+	function slideMain() {
+		var $slide = $('.main-wrapper .slide')
+		var $pagerSlide = $('.main-wrapper .pager-slide')
+		var video = $('.main-wrapper .video')[0]
+		var len = $slide.length
+		var lastIdx = len - 1
+		var depth = 2
+		var idx = 0
+		var gap = 5000
+		var speed = 500
+		var timeout
+
+		function onPagerClick() {
+			idx = $(this).index()
+			onPlay('pager')
+		}
+
+		function onModalVideoClose() {
+			$('.modal-video').hide()
+		}
+
+		function onModalVideo() {
+			$('.modal-video').show()
+		}
+
+		function onLoadedVideo() {
+			if (video.readyState >= 2) {
+				video.playbackRate = 4.0
+			}
+		}
+
+		function ani() {
+			$(this).addClass('active')
+			video.currentTime = 0
+			if ($slide.eq(idx).hasClass('is-video')) video.play()
+			else {
+				clearTimeout(timeout)
+				timeout = setTimeout(onPlay, gap)
+			}
+		}
+
+		function onPlay(state) {
+			if (state !== 'pager') idx = (idx == lastIdx) ? 0 : idx + 1;
+			$pagerSlide.find('.pager').removeClass('active')
+			$pagerSlide.find('.pager').eq(idx).addClass('active')
+			$slide.eq(idx).css({
+				'z-index': depth++,
+				'left': '100%'
+			})
+			$slide.removeClass('active')
+			$slide.eq(idx).stop().animate({
+				'left': 0
+			}, speed, ani)
+		}
+
+
+
+		$slide.eq(idx).css('z-index', depth++)
+		$slide.eq(idx).addClass('active')
+		for (var i = 0; i < len; i++) {
+			$('<i class="pager" ></i>').appendTo($pagerSlide).click(onPagerClick).addClass((idx === i) ? 'active' : '')
+
+			video.addEventListener('loadeddata', onLoadedVideo)
+			video.addEventListener('ended', onPlay)
+			$('.bt-video').click(onModalVideo)
+			$('.modal-video').find('.bt-close').click(onModalVideoClose)
+			$('.pager-wrapper .pager').click(function () {
+				$('.pager-wrapper .pager').removeClass('active')
+				$(this).addClass('active')
+			})
+
+			ani()
+		}
+	}
+
+	function slideDream() {
+		var swiper = new Swiper('.dream-wrapper .swiper-container', {
+			);
+		}
+
+		function slidePromo() {
+			var $promoWrapper = $('.promo-wrapper')
+			var $slideWrap = $promoWrapper.find('.slide-wrap')
+
+			function onGetData(r) {
+				r.promo.forEach(function (v, i) {
+					var html = ''
+					html += '<li class="slide swiper-slide">';
+					html += '<div class="img-wrap">';
+					html += '<img src="' + v.src + '" alt="메뉴" class="w-100">';
+					html += '</div>';
+					html += '<div class="cont-wrap">';
+					html += '<h3 class="title">' + v.title + '</h3>';
+					html += '<div class="desc">' + v.desc + '</div>';
+					html += '</div>';
+					html += '</li>';
+					$slideWrap.append(html)
+				})
+				var swiper = new Swiper('.promo-wrapper .swiper-container', )
+			}
+
+			$.get('../json/promotion.json', onGetData)
+		}
+
+
+
+
+
+
 	})
-
-
-
-	//*************** 이벤트 콜백 *****************/
-	function onCloseTodayCookie() {
-		$.cookie('hideCookie', 'Y', {
-			expires: 1,
-			path: '/'
-		});
-		onCloseCookie();
-	}
-
-	function onGetWeather(r) {
-		$weather.find('.icon').addClass(weatherIcon['i' + r.weather[0].icon])
-		$weather.find('.temp').text(Math.floor(r.main.temp))
-		$weather.find('.date').text(moment(r.dt * 1000).format('YYYY. M. D. ddd'))
-		$weather.find('.time > span').text(moment(r.dt * 1000).format('hh:mm'))
-		$weather.find('.time > small').text(moment(r.dt * 1000).format('A'))
-	}
-
-	function onGetGeo(r) {
-		weatherData.lat = r.coords.latitude
-		weatherData.lon = r.coords.longitude
-		$.get(weatherURL, weatherData, onGetWeather)
-	}
-
-	function onErrorGeo() {
-		weatherData.let = 33.4857397138786
-		weatherData.llon = 126.48154043372092
-		$.get(weatherURL, weatherData, onGetWeather)
-	}
-
-	function onPagerClick() {
-		idx = $(this).index()
-		onPlay('pager')
-	}
-
-	function onCloseCookie() {
-		$('.cookie-wrapper').hide()
-	}
-
-	function onModalVideoClose() {
-		$('.modal-video').hide()
-	}
-
-	function onModalVideo() {
-		$('.modal-video').show()
-	}
-
-	function onLoadedVideo() {
-		if (video.readyState >= 2) {
-			video.playbackRate = 4.0
-		}
-	}
-
-	function ani() {
-		$(this).addClass('active')
-		video.currentTime = 0
-		if ($slide.eq(idx).hasClass('is-video')) video.play()
-		else {
-			clearTimeout(timeout)
-			timeout = setTimeout(onPlay, gap)
-		}
-	}
-
-	function onPlay(state) {
-		if (state !== 'pager') idx = (idx == lastIdx) ? 0 : idx + 1;
-		$pagerSlide.find('.pager').removeClass('active')
-		$pagerSlide.find('.pager').eq(idx).addClass('active')
-		$slide.eq(idx).css({
-			'z-index': depth++,
-			'left': '100%'
-		})
-		$slide.removeClass('active')
-		$slide.eq(idx).stop().animate({
-			'left': 0
-		}, speed, ani)
-	}
-
-
-
-
-
-
-
-
-
-
-})
